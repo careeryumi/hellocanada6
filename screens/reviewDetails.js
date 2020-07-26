f5bb4b76dbdf6972b268e3da52a9abfda561a22c
Yumi <careeryumi@gmail.com>
2020-07-26 05:01:56 -0400

    navigation and adding forum

---
+++ screens/reviewDetails.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { StyleSheet, View, Text, Image } from 'react-native';
+import { globalStyles, images } from '../styles/global';
+import Card from '../shared/card';
+
+export default function ReviewDetails({ navigation }) {
+  const rating = navigation.getParam('rating');
+  
+  return (
+    <View style={globalStyles.container}>
+      <Card>
+        <Text style={globalStyles.titleText}>
+          { navigation.getParam('title') }
+        </Text>
+        <Text>{ navigation.getParam('body') }</Text>
+        <View style={styles.rating}>
+          <Text>Image: </Text>
+          <Image source={images.ratings[rating]} />
+          {/* <Image source={require('../assets/rating-1.png')} />
+          <Image source={require('../assets/rating-' + navigation.getParam('rating')+'.')} /> */}
+        </View>
+      </Card>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  rating: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingTop: 16,
+    marginTop: 16,
+    borderTopWidth: 1,
+    borderTopColor: '#eee',
+  }
+});
